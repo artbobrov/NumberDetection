cc38a96a518faa03441890a58805e392f294f338
Unknown <artembobrov3059@gmail.com>
2017-10-09 14:04:15 +0200

    added prediction with cnn

--- NumberDetection/PaintView.swift
+++ NumberDetection/PaintView.swift
@@ -12,6 +12,26 @@ class PaintView: UIImageView {
 	var location: CGPoint!
 	var delegate: PaintViewDelegate?
 
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		commonInit()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		commonInit()
+	}
+
+
+	private func commonInit() {
+//		self.backgroundColor = .white
+	}
+//	override func draw(_ rect: CGRect) {
+//		super.draw(rect)
+//		self.backgroundColor = .white
+//	}
+
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 		guard let touch = touches.first else {
 			return
@@ -62,6 +82,29 @@ class PaintView: UIImageView {
 		UIGraphicsEndImageContext();
 		location = currentLocation;
 		delegate?.drawingEnded()
+	}
+
+
+	func getViewContext() -> CGContext? {
+		let colorSpace:CGColorSpace = CGColorSpaceCreateDeviceGray()
+		let bitmapInfo = CGImageAlphaInfo.none.rawValue
+		guard let context = CGContext(data: nil,
+		                              width: 28,
+		                              height: 28,
+		                              bitsPerComponent: 8,
+		                              bytesPerRow: 28,
+		                              space: colorSpace,
+		                              bitmapInfo: bitmapInfo)
+			else {
+				fatalError("Could not get context")
+		}
+
+		context.translateBy(x: 0 , y: 28)
+		context.scaleBy(x: 28/self.frame.size.width, y: -28/self.frame.size.height)
+
+		self.layer.render(in: context)
+
+		return context
 	}
 }
 
--- NumberDetection/ViewController.swift
+++ NumberDetection/ViewController.swift
@@ -14,17 +14,35 @@ class ViewController: UIViewController, PaintViewDelegate{
 	@IBOutlet weak var paintView: PaintView!
 	@IBOutlet weak var inputImage: UIImageView!
 
+	@IBOutlet weak var predictionLabel: UILabel!
+	@IBOutlet weak var previewImage: UIImageView!
+
+	let model = MNIST()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		paintView.delegate = self
+		previewImage.isHidden = true
 	}
 
 	func drawingEnded() {
-
+		if let imgpixbuf = paintView.image?.resize(to: CGSize(width: 28.0, height: 28.0)).pixelBuffer(){
+			previewImage.isHidden = false
+			predictionLabel.isHidden = false
+			previewImage.image = UIImage(ciImage: CIImage(cvImageBuffer: imgpixbuf))
+			let prediction = try? self.model.prediction(image: imgpixbuf)
+			guard let predictionValue = prediction else {
+				self.predictionLabel.text = ""
+				return
+			}
+			self.predictionLabel.text = "It is \(predictionValue.classLabel) with confidence \(predictionValue.prediction[predictionValue.classLabel] ?? -1)"
+		}
 	}
 
 	@IBAction func clearButtonClicked(_ sender: UIButton) {
 		paintView.image = UIImage()
+		previewImage.isHidden = true
+		predictionLabel.isHidden = true
 	}
 }
 
